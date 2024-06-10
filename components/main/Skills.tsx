4e16a223f74ccea147576a2649ac9ba9d66c83aa
nittin-murthi <106542436+nittin-murthi@users.noreply.github.com>
2024-06-10 00:43:11 -0500

    Update Skills.tsx

--- components/main/Skills.tsx
+++ components/main/Skills.tsx
@@ -98,7 +98,7 @@ const Skills = () => {
                     loop
                     muted
                     autoPlay
-                    src="/cards-video.webm"
+                    src="./cards-video.webm"
                     />
                 </div>
             </div>
